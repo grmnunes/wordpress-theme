b755949eec834aca9b95a285bd184f82da435335
Germano Nunes <grmnunes@gmail.com>
2021-07-26 07:24:20 -0300

    Delete post-related.php

--- gamestore/post-related.php
+++
@@ -1,6 +0,0 @@
-<div class="row">
-    <div class="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center">
-        <h6> <a class="text-decoration-none text-dark" href="<?php the_permalink() ?>"><?php the_title() ?> </a> </h4>
-        <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium', array('class' => 'img-fluid')) ?> </a>
-    </div>
-</div>
